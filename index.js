f2f31149ae96bd9b8f1347909ce8c4e65448bf82
Nagaraju Bhusani <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-06-30 20:58:17 +0530

    added changes in circular module

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.set('port', (process.env.PORT || 5000));
 app.post('/', function(req, response) {
     //response.render('pages/index');
     var request = CircularJSON.stringify(req);
-    console.log('Client Request=====>'+request);
+    console.log('Client Request=====>'+request.body.request.intent.name);
     response.status(200).json(resp);
 
     //response.writeHead("200, {'Content-Type': 'text/html'}");
