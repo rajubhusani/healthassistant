7befd1a802ca69181b412516c14eaea5815e19ce
Nagaraju Bhusani <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-06-30 20:12:01 +0530

    added changes in index.js

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
-    //console.log('Node app is running on port');
-    //response.status(200).json(JSON.stringify({'message':'Hello Test'}));
+    //response.render('pages/index');
+    console.log('Node app is running on port');
+    response.status(200).json(JSON.stringify({'message':'Hello Test'}));
 
     //response.writeHead("200, {'Content-Type': 'text/html'}");
     //response.send(JSON.stringify(resp));
