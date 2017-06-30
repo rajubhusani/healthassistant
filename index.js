6de64953e34b246c3b9312ebc86f4a6226789a3f
Nagaraju Bhusani <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-06-30 18:57:12 +0530

    node.js changes

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 var express = require('express');
 var app = express();
 
+var resp = {
+  "version": "1.0",
+  "response": {
+    "outputSpeech": {
+      "type": "SSML",
+      "ssml": "<speak><p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p></speak>"
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "SSML",
+        "ssml": "<speak><p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p></speak>"
+      }
+    },
+    "speechletResponse": {
+      "outputSpeech": {
+        "ssml": "<speak><p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p></speak>"
+      },
+      "reprompt": {
+        "outputSpeech": {
+          "ssml": "<speak><p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p></speak>"
+        }
+      },
+      "shouldEndSession": false
+    }
+  },
+  "sessionAttributes": {
+    "SayMsgData": "<p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p>",
+    "rePromtData": "<p>You can say, what are my activities, record a measurement, or send a caregiver a message. </p>",
+    "requireLastIntent": false
+  }
+};
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +46,7 @@ app.get('/', function(request, response) {
   //console.log('Node app is running on port', app.get('port'));
 
   response.writeHead("200, {'Content-Type': 'text/html'}");
-  response.end('thanks');
+  response.end(resp);
 });
 
 app.listen(app.get('port'), function() {
