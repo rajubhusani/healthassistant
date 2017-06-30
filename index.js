8763a1c142bf695915ffdcbf2c25d2d241e609f2
Nagaraju Bhusani <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-06-30 19:52:54 +0530

    added changes in index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     //response.render('pages/sample');
