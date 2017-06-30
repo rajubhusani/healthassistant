24342f4bc4c0a6519aa6971dea0ed7ae64c82f5c
Nagaraju Bhusani <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-06-30 19:48:31 +0530

    added changes in index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
+ var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 
-app.use(express.static(__dirname + '/public'));
+//app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     //response.render('pages/sample');
-    response.status(200).json(JSON.stringify({'message':'Hello Test'});
+    response.status(200).json(JSON.stringify({'message':'Hello Test'}));
     //console.log('Node app is running on port', app.get('port'));
 
     //response.writeHead("200, {'Content-Type': 'text/html'}");
