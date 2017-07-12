370d44c8fd9ebda71c25979bb1c3987474fffb55
Aditya Kotamraju <aditya.kotamraju@cognizant.com>
2017-07-12 11:52:29 +0530

    Modified to accept LaunchIntent

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongodb = require("mongodb");
 
 var MONGO_CONNECTION_URL = 'mongodb://adityakotamraju:Health123@ds151752.mlab.com:51752/health_assistant';
 var COLLECTION = {
-    "USERS" : "Users",
+    "USERS": "Users",
     "REMINDERS": "Reminders"
 };
 
@@ -20,20 +20,20 @@ var db;
 var alexa = require("./alexa/alexa_response");
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(MONGO_CONNECTION_URL, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+mongodb.MongoClient.connect(MONGO_CONNECTION_URL, function(err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-  db = database;
-  console.log("Database connected to Health Assistant");
+    db = database;
+    console.log("Database connected to Health Assistant");
 });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({ "error": message });
 }
 
 /*  "/app/login"
@@ -49,22 +49,22 @@ app.get("/", function(req, res) {
  */
 
 app.post("/app/login", function(req, res) {
-  db.collection(COLLECTION.USERS).find({
-      $and: [
-        {email : req.body.username},
-        {password: req.body.password}
-      ]
-  }).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Invalid username and password");
-    } else {
-      if(docs.length > 0) {
-        res.status(200).json(docs);
-      } else{
-        handleError(res, "", "Invalid username and password");
-      }
-    }
-  });
+    db.collection(COLLECTION.USERS).find({
+        $and: [
+            { email: req.body.username },
+            { password: req.body.password }
+        ]
+    }).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Invalid username and password");
+        } else {
+            if (docs.length > 0) {
+                res.status(200).json(docs);
+            } else {
+                handleError(res, "", "Invalid username and password");
+            }
+        }
+    });
 });
 
 app.post("/app/schedule", function(req, res) {
@@ -72,23 +72,24 @@ app.post("/app/schedule", function(req, res) {
     var newSchedule = req.body;
 
     if (!req.body.type) {
-      handleError(res, "Invalid user input", "Must provide the type of reminder", 400);
+        handleError(res, "Invalid user input", "Must provide the type of reminder", 400);
     }
 
     db.collection(COLLECTION.REMINDERS).insertOne(newSchedule, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to schedule a reminder.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
+        if (err) {
+            handleError(res, err.message, "Failed to schedule a reminder.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
 
 });
 
-app.post("/alexa", function(req, res){
-    if(req.body.intent.type === "LaunchIntent") {
-      var resp = alexa.sayHello();
-      res.status(200).json(resp);
+app.post("/alexa", function(req, res) {
+    console.log('Received request from alexa..!');
+    if (req.body.request.type === "LaunchIntent") {
+        var resp = alexa.sayHello();
+        res.status(200).json(resp);
     }
 });
 
