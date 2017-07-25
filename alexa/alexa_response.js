471bdbfb8b295d39c848f1b431a0517a3bbec154
414506 <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-07-25 20:21:20 +0530

    updated dynamic SSML

--- alexa/alexa_response.js
+++ alexa/alexa_response.js
@@ -1,38 +1,6 @@
 var alexa = module.exports = {};
 var format = require('string-format');
 
-var resp = {
-    "version": "1.0",
-    "response": {
-        "outputSpeech": {
-            "type": "SSML",
-            "ssml": "<speak><p>Nagaraju, welcome back to HealthActivate, You have a medication reminder, <break strength='none' time='1s'/> at 08:20 AM you are scheduled to take your ACE medication.<break strength='none' time='750ms'/> How may I help you? </p></speak>"
-        },
-        "reprompt": {
-            "outputSpeech": {
-                "type": "SSML",
-                "ssml": "<speak><p>You can say, what are my goals for today, what is my medication schedule, or message my health coach.</p></speak>"
-            }
-        },
-        "speechletResponse": {
-            "outputSpeech": {
-                "ssml": "<speak><p>Nagaraju, welcome back to HealthActivate, You have a medication reminder, <break strength='none' time='1s'/> at 08:20 AM you are scheduled to take your ACE medication.<break strength='none' time='750ms'/> How may I help you? </p></speak>"
-            },
-            "reprompt": {
-                "outputSpeech": {
-                    "ssml": "<speak><p>You can say, what are my goals for today, what is my medication schedule, or message my health coach.</p></speak>"
-                }
-            },
-            "shouldEndSession": false
-        }
-    },
-    "sessionAttributes": {
-        "SayMsgData": "<p>Nagaraju, welcome back to HealthActivate, You have a medication reminder, <break strength='none' time='1s'/> at 08:20 AM you are scheduled to take your ACE medication.<break strength='none' time='750ms'/>  How may I help you? </p>",
-        "rePromtData": "<p>You can say, what are my goals for today, what is my medication schedule, or message my health coach.</p>",
-        "requireLastIntent": false
-    }
-};
-
 alexa.sayHello = function(username) {
     var speakText = format("<speak><p>Hi {}, welcome to HealthAssistant, <break strength='none' time='1s'/> How may I help you? </p></speak>", username);
 
@@ -101,7 +69,7 @@ alexa.getSSMLResponse = function(dynamicText, isEndSession, requireLastIntent) {
             }
         },
         "sessionAttributes": {
-            "SayMsgData": "<p>Nagaraju, welcome back to HealthActivate, You have a medication reminder, <break strength='none' time='1s'/> at 08:20 AM you are scheduled to take your ACE medication.<break strength='none' time='750ms'/>  How may I help you? </p>",
+            "SayMsgData": dynamicText,
             "rePromtData": "<p>You can say, what are my goals for today, what is my medication schedule, or message my health coach.</p>",
             "requireLastIntent": requireLastIntent
         }
