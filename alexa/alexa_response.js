c2dbf35ee85d8a94f983928b6890044d4c4e814d
414506 <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-07-25 21:08:34 +0530

    updated dynamic SSML

--- alexa/alexa_response.js
+++ alexa/alexa_response.js
@@ -21,7 +21,7 @@ alexa.readData = function(taskResponse, measurement) {
     var response = JSON.stringify(taskResponse);
     var alexaText = "<speak><p>You have not captured your readings.. <break strength='none' time='750ms'/> Is there anything else I can help with? </p></speak>";
     if (!taskResponse || taskResponse.length !== 0) {
-        alexaText = "<speak><p>Your " + measurement + " value is " + taskResponse[0].healthdata[0].value + "<break strength='none' time='750ms'/> Is there anything else I can help with? </p></speak>";
+        alexaText = "<speak>Your " + measurement + " value is " + taskResponse[0].healthdata[0].value + "<break strength='none' time='750ms'/> Is there anything else I can help with? </speak>";
         //alexaText += alexa.advice(taskResponse[0].healthdata[0].value, measurement);
     }
 
