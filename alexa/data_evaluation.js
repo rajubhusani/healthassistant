cab702b815788c39ba08616eec21355cdf530155
414506 <NagarajuBhusani@Nagarajus-Mac-mini.local>
2017-07-26 19:59:02 +0530

    updated tips

--- alexa/data_evaluation.js
+++ alexa/data_evaluation.js
@@ -10,18 +10,18 @@ dataEvaluator.evaluate = function(userid, value, type, dataBase) {
             var diastolic = value.split("/")[1];
             //HIGHBP
             if (systolic > 140 || diastolic > 90) {
-                tip += "Reduce the salt in your diet<break strength='none' time='500ms'/>Eat more fruits, vegetables, grains, and low-fat dairy foods";
+                tip = "Reduce the salt in your diet<break strength='none' time='500ms'/>Eat more fruits, vegetables, grains, and low-fat dairy foods";
             }
             //LOWBP 
             else if (systolic < 90 || diastolic < 60) {
-                tip += "Eat a diet higher in salt<break strength='none' time='500ms'/>Get regular exercise to promote blood flow";
+                tip = "Eat a diet higher in salt<break strength='none' time='500ms'/>Get regular exercise to promote blood flow";
             }
             break;
         case "blood sugar":
             if (value > 180) {
-                tip += "Eliminate wheat in your diet <break strength='none' time='750ms'/> Check your stress level</p></speak>";
+                tip = "Eliminate wheat in your diet <break strength='none' time='750ms'/> Check your stress level</p></speak>";
             } else if (value < 70) {
-                tip += "Very low blood sugar is a medical emergency";
+                tip = "Very low blood sugar is a medical emergency";
             }
             break;
     }
